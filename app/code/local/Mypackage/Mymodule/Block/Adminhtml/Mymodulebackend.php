36fd5940f92f244c433b80d08d7a7208b1132f79
Elavarasann <ela.youngking@gmail.com>
2014-04-30 19:47:24 +0530

    removed duplicate directories

--- app/code/local/Mypackage/Mymodule/Block/Adminhtml/Mymodulebackend.php
+++
@@ -1,5 +0,0 @@
-<?php  
-
-class Mypackage_Mymodule_Block_Adminhtml_Mymodulebackend extends Mage_Adminhtml_Block_Template {
-
-}
--- app/code/local/Mypackage/Mymodule/Block/Index.php
+++
@@ -1,8 +0,0 @@
-<?php   
-class Mypackage_Mymodule_Block_Index extends Mage_Core_Block_Template{   
-
-
-
-
-
-}
